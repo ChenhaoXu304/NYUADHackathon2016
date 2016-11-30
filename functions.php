5f80c260f874d0ba0ada489b26de42931721211e
Chenhao Xu <cx473@nyu.edu>
2016-11-30 20:39:31 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,5 +1,10 @@
 <?php 
+function redirect_to($new_location) {
+	  header("Location: " . $new_location);
+	  exit;
+	}
 function create_user($username,$password){
+  global $connection;
   $query="INSERT INTO users ";
   $query.="(username, hashed_password) ";
   $query.="VALUES ('{$username}', '{$password}')";
@@ -7,25 +12,32 @@ function create_user($username,$password){
 }
 
 function login($username,$password){
+  global $connection;
   $query="SELECT * FROM users ";
   $query.="WHERE username='{$username}' ";
   $result=mysqli_query($connection,$query);
   if($result_list=mysqli_fetch_assoc($result)){
     if($result_list["hashed_password"]==$password){
-      $SESSION['user']=$result_list
-      redirect_to("index.html")
+      $SESSION['user']=$result_list;
+      redirect_to("index.html");
     }else{
-      $SESSION["message"]="User password doesn't match."
+      $SESSION["message"]="User password doesn't match.";
     }
     }else{
-    $SESSION['message']="User not found."
+    $SESSION['message']="User not found.";
   }
  
 }
 function find_rides($number){
-
+  global $connection;
+  $query="SELECT * FROM requests ";
+  $query.="ORDER BY time ";
+  $query.="LIMIT {$number}";
+  $result= mysqli_query($connection,$query);
+  return $query;
 }
 function filter_rides($number, $start, $destination, $time, $pass_num){
+  global $connection;
 
 }
 ?>
