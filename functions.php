f10e156eecae631d5a59415dba7405f71fed14c5
Chenhao Xu <cx473@nyu.edu>
2016-11-30 21:28:33 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,3 +1,32 @@
+<?php
+
+	session_start();
+	
+	function message() {
+		if (isset($_SESSION["message"])) {
+			$output = "<div class=\"message\">";
+			$output .= htmlentities($_SESSION["message"]);
+			$output .= "</div>";
+			
+			// clear message after use
+			$_SESSION["message"] = null;
+			
+			return $output;
+		}
+	}
+
+	function errors() {
+		if (isset($_SESSION["errors"])) {
+			$errors = $_SESSION["errors"];
+			
+			// clear message after use
+			$_SESSION["errors"] = null;
+			
+			return $errors;
+		}
+	}
+	
+?>
 <?php 
 function redirect_to($new_location) {
 	  header("Location: " . $new_location);
