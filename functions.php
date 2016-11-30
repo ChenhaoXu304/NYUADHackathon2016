7b95079063b6856a60fe0b575d32c919aace34a6
Chenhao Xu <cx473@nyu.edu>
2016-11-30 20:33:23 +0800

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,12 +1,26 @@
 <?php 
 function create_user($username,$password){
   $query="INSERT INTO users ";
-  $query.="()"
-    
+  $query.="(username, hashed_password) ";
+  $query.="VALUES ('{$username}', '{$password}')";
+  $result=mysqli_query($connection,$query);
 }
 
 function login($username,$password){
-
+  $query="SELECT * FROM users ";
+  $query.="WHERE username='{$username}' ";
+  $result=mysqli_query($connection,$query);
+  if($result_list=mysqli_fetch_assoc($result)){
+    if($result_list["hashed_password"]==$password){
+      $SESSION['user']=$result_list
+      redirect_to("index.html")
+    }else{
+      $SESSION["message"]="User password doesn't match."
+    }
+    }else{
+    $SESSION['message']="User not found."
+  }
+ 
 }
 function find_rides($number){
 
