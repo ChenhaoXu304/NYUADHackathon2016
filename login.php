282f17e8b2d0fb21d5b11384569e3ee66f59eb1a
Mai Le Xuan Anh <alm818@nyu.edu>
2016-11-30 17:06:07 +0400

    Update login.php

--- login.php
+++ login.php
@@ -1,15 +1,25 @@
+<?php include("functions.php");?>
+<?php include("db_connection.php");?>
+<?php
+	if($_POST["submit"]){
+		login($POST["username"],$POST["password"]);
+	}
+?>
 <!DOCTYPE html>
 <html>
 <head>
 	<title>LOGIN</title>
-	<form action="db_connection.php">
+	<?if (isset($_SESSION["message"]){
+		echo $_SESSION["message"];
+	})?>
+	<form action=login.php">
 		User name:<br>
 		<input type="text" name="username">
   		<br>
   		Password:<br>
   		<input type="text" name="password">
   		<br><br>
-  		<input type="submit" value="Login">
+  		<input type="submit" name="submit" value="Login">
 	</form> 
 
 </head>
