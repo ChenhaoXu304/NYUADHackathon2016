52226adbdf4a860d4da96866c31a374de0a530d9
Mai Le Xuan Anh <alm818@nyu.edu>
2016-11-30 16:54:31 +0400

    Add files via upload

---
+++ login.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>LOGIN</title>
+	<form action="db_connection.php">
+		User name:<br>
+		<input type="text" name="username">
+  		<br>
+  		Password:<br>
+  		<input type="text" name="password">
+  		<br><br>
+  		<input type="submit" value="Login">
+	</form> 
+
+</head>
+<body>
+
+</body>
+</html>
